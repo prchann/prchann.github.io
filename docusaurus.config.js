1d898ccd6e131a289a8b30dd63ca251b713d8f76
perrychann <17397116+perrychan1@users.noreply.github.com>
2022-08-13 17:03:01 +0800

    add CNAME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Hippy',
   tagline: 'Dinosaurs are cool',
-  url: 'https://hippy.cc/',
+  url: 'https://www.hippy.cc/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
