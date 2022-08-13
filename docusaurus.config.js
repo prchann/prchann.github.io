989221a2b2a166fec1e8a7a7ab93f2c61005bcba
perrychann <17397116+perrychan1@users.noreply.github.com>
2022-08-13 17:16:49 +0800

    change default domain to hippy.cc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Hippy',
   tagline: 'Dinosaurs are cool',
-  url: 'https://www.hippy.cc/',
+  url: 'https://hippy.cc/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
